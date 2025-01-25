bdc77e5867864fda0bda1584e616a862fecd4b24
r1y9o80 <Ryo-official@outlook.jp>
2025-01-25 11:33:12 +0900

    Update close_talk.js

--- close_talk.js
+++ close_talk.js
@@ -7,7 +7,7 @@ let PosiData_FromId = {}
 console.log(WebSocket);
 
 // ローカルサーバーを立ち上げる
-const WebSocketServer = new WebSocket.Server({ port: 19131 }); // WebSocketサーバーをポート19131で開始
+const WebSocketServer = new WebSocket.Server({ port: process.env.PORT || 19131 }); // WebSocketサーバーをポート19131で開始
 
 // マイクラとサーバーの接続を検知
 WebSocketServer.on("connection", (socket) => {
