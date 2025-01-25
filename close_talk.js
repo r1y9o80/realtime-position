08982fc6fbdef05a9f9646dec1a2abe56f47e016
r1y9o80 <Ryo-official@outlook.jp>
2025-01-25 13:42:59 +0900

    OK

--- close_talk.js
+++ close_talk.js
@@ -3,18 +3,18 @@
 const express = require("express")
 const app = express();
 const http = require("http");
-const SV = http.createServer(app);
+const server = http.createServer(app);
 const PORT = process.env.PORT || 8000;
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
 
-SV.listen(PORT, () => {
-    console.log(`HTTPサーバーがポート ${httpPort} で起動しました`);
+server.listen(PORT, () => {
+    console.log(`HTTPサーバーがポート ${Port} で起動しました`);
 });
 
 // HTTPサーバーエラーハンドリング
-SV.on('error', (error) => {
+server.on('error', (error) => {
     console.error('HTTPサーバーエラー:', error);
 });
