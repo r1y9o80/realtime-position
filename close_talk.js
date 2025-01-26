4a4d9ea47c4edf4d0f15dcfd150715e70ead14a3
r1y9o80 <Ryo-official@outlook.jp>
2025-01-26 21:12:22 +0900

    OK

--- close_talk.js
+++ close_talk.js
@@ -105,5 +105,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/index.html")
-})
+    res.sendFile(__dirname + "/index.html");
+});
+
+
