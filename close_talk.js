680eef204504e4e2079a3bcf06cfcb55b0f4718b
r1y9o80 <Ryo-official@outlook.jp>
2025-01-27 02:12:38 +0900

    OK

--- close_talk.js
+++ close_talk.js
@@ -54,7 +54,7 @@ WebSocketServer.on("connection", (socket) => {
             const return_data = JSON.parse(rawData);
             // プレイヤー移動イベントの処理
             if (return_data.header.eventName === 'PlayerTravelled') {
-                sock_Name[socket] = body.player.name
+                sock_Name[socket] = return_data.body.player.name
                 user_data[return_data.body.player.name] = { exist: true, Posi: return_data.body.player.position };
                 console.log("プレイヤー位置:", user_data);
             }
